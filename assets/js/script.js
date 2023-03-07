f0b831fdc5a1e51f8f8c18172aa800b505b44f79
Shane Wade <shanebramble96@hotmail.com>
2023-03-06 18:52:16 -0600

    implemnet datepicker ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,12 +93,21 @@ $(".list-group").on("click", "span", function () {
   // swap out element
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker.
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the 'dataInput'.
+      $(this).trigger("change");
+    },
+  });
+
   // automatically focus on new element.
   dateInput.trigger("focus");
 });
 
 // value of due date was changed.
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this).val().trim();
 
@@ -133,16 +142,16 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    // console.log("activate", this);
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    // console.log("deactivate", this);
   },
   over: function (event) {
-    console.log("over", event.target);
+    // console.log("over", event.target);
   },
   out: function (event) {
-    console.log("out", event.target);
+    // console.log("out", event.target);
   },
   update: function (event) {
     // array to store the task data in.
@@ -171,7 +180,11 @@ $(".card .list-group").sortable({
     saveTasks();
   },
 });
+$("#modalDueDate").datepicker({
+  minDate: 1,
+});
 
+// red drop area to remove list items.
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
